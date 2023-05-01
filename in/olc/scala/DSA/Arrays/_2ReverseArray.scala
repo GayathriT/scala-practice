ac5f00d040cd370fd685c167c6ff2a909c77d419
GayathriT <GayathriT@users.noreply.github.com>
2023-05-01 20:02:29 +0530

    Create _2ReverseArray.scala

---
+++ in/olc/scala/DSA/Arrays/_2ReverseArray.scala
@@ -0,0 +1,20 @@
+package Arrays
+
+object _1ReverseArray {
+    val input=Array(1,3,4,5,2,9,7,8)
+     def main(args: Array[String]) {
+
+       val res2 = reversArr_1(input)
+      println("Method 2")
+      res2.map(e => print(s"${e} || "))
+      println
+
+
+    }
+    
+    def reversArr_1(inputArr: Array[Int]) = {
+      inputArr.zipWithIndex.sortWith(_._2 > _._2)
+    }
+    
+    
+}
